2463d2766ccc853441e277d2f4d357c19c33ae89
Aaron Hammond <ahammond@mit.edu>
2013-10-05 21:58:13 -0400

    everything is working~wooooo
    baseline functionality that i am okay with

--- models/User.js
+++ models/User.js
@@ -15,8 +15,7 @@ var SALT_WORK_FACTOR = 10;
 
 var UserSchema = new Schema({
     username: {type: String, required: true, index : { unique: true} },
-    password: { type: String, required: true },
-    profile_image : {type: String, required: true}
+    password: { type: String, required: true }
 });
 
 /*
